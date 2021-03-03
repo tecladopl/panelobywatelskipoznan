a6d3eb55d0a1a318d6172acb1f40b97b04165b3c
tecladopl <67960327+tecladopl@users.noreply.github.com>
2021-03-03 14:27:16 +0100

    Update panel_poznan.r

--- panel_poznan.r
+++ panel_poznan.r
@@ -6,8 +6,8 @@ library(GenSA)
 
 
 # Do zmiennej "poznan" wczytujemy dane z pliku CSV
-setwd('/home/kozun/PRACA/panele/losowania_final')
-poznan <- read.csv(file = 'poznan_1.csv')
+setwd('/')
+poznan <- read.csv(file = 'poznan.csv')
 
 #liczba obos
 N <- 65
@@ -86,7 +86,7 @@ funkcja_oceniajaca <- function(v,b) {
   
   ret <- 0
   
-  
+  #zapobieganie duplikatom
   if(any(duplicated(poznan$ID[v]))){
     ret <- 99999999
   }else{
@@ -104,16 +104,19 @@ funkcja_oceniajaca <- function(v,b) {
     man65 <- 0
     woman65 <- 0
     
+    #liczniki wyksztalcenia
     primary <- 0	
     medium <- 0
     higher <- 0
     
+    #liczniki stosunku klimatu
     c1 <- 0
     c2 <- 0
     c3 <- 0
     c4 <- 0
     c5 <- 0
     
+    #liczniki osiedla
     a1 <- 0
     a2 <- 0
     a3 <- 0
@@ -288,14 +291,17 @@ funkcja_oceniajaca <- function(v,b) {
       }
     }
     
+    #do wieku i plci dodana jest waga aby zwiekszyc nacisk na te kryteria
     ret <- ret + (N_MAN18-man18)^2*10 + (N_WOMAN18-woman18)^2*10 + (N_MAN25-man25)^2*10 + (N_WOMAN25-woman25)^2*10 + (N_MAN40-man40)^2*10 + (N_WOMAN40-woman40)^2*10 + (N_MAN65-man65)^2*10 + (N_WOMAN65-woman65)^2*10 + (N_PRIMARY-primary)^2 + (N_MEDIUM-medium)^2 + (N_HIGHER-higher)^2 + (NC_1 -c1)^2 +(NC_2-c2)^2+(NC_3-c3)^2+(NC_4-c4)^2+(NC_5-c5)^2+(N_A_1-a1)^2+(N_A_2-a2)^2+(N_A_3-a3)^2+(N_A_4-a4)^2+(N_A_5-a5)^2+(N_A_6-a6)^2+(N_A_7-a7)^2+(N_A_8-a8)^2+(N_A_9-a9)^2+(N_A_10-a10)^2+(N_A_11-a11)^2+(N_A_12-a12)^2+(N_A_13-a13)^2+(N_A_14-a14)^2+(N_A_15-a15)^2+(N_A_16-a16)^2+(N_A_17-a17)^2+(N_A_18-a18)^2+(N_A_19-a19)^2+(N_A_20-a20)^2+(N_A_21-a21)^2+(N_A_22-a22)^2+(N_A_23-a23)^2+(N_A_24-a24)^2+(N_A_25-a25)^2+(N_A_26-a26)^2+(N_A_27-a27)^2+(N_A_28-a28)^2+(N_A_29-a29)^2+(N_A_30-a30)^2+(N_A_31-a31)^2+(N_A_32-a32)^2+(N_A_33-a33)^2+(N_A_34-a34)^2+(N_A_35-a35)^2+(N_A_36-a36)^2+(N_A_37-a37)^2+(N_A_38-a38)^2+(N_A_39-a39)^2+(N_A_40-a40)^2+(N_A_41-a41)^2+(N_A_42-a42)^2
     
   }
   
   if (b) {
+    #prezentacja wynikow
     print( poznan$Code[v] )
     print( cat('man18=', man18, ' woman18=', woman18,'man25=', man25, ' woman25=', woman25,'man40=', man40, ' woman40=', woman40,'man65=', man65, ' woman65=', woman65, ' primary=', primary, ' medium=', medium, ' higher=', higher,' climate1=',c1,' climate2=',c2,' climate3=',c3, ' cliamte4=',c4, ' climate5=',c5,' area1=',a1,' area2=',a2,' area3=',a3,' area4=',a4,' area5=',a5,' area6=',a6,' area7=',a7,' area8=',a8,' area9=',a9,' area10=',a10,' area11=',a11,' area12=',a12,' area13=',a13,' area14=',a14,' area15=',a15,' area16=',a16,' area17=',a17,' area18=',a18,' area19=',a19,' area20=',a20,' area21=',a21,' area22=',a22,' area23=',a23,' area24=',a24,' area25=',a25,' area26=',a26,' are27=',a27,' area28=',a28,' area29=',a29,' area30=',a30,' area31=',a31,' area32=',a32,' area33=',a33,' area34=',a34,' area35=',a35,' area36=',a36,' area37=',a37,' area38=',a38,' area39=',a39,' area40=',a40,' area41=',a41,' area42=',a42) )
-    write.csv(cbind(poznan$ID[v],poznan$Code[v]),'wynik_1.csv',row.names = FALSE)
+   #zapis wynikow do pliku
+    write.csv(cbind(poznan$ID[v],poznan$Code[v]),'wynik.csv',row.names = FALSE)
   }
   
   ret
@@ -306,18 +312,10 @@ funkcja_oceniajaca <- function(v,b) {
 # Argumentem jest wektor danych (lista ID osob) - poniewaz nam chodzi o zbior 65 osob, wiec poprzez wartosc funkcji
 # musimy wykluczyc wektory, gdzie ID sie powtarzaja (damy im bardzo wysoka wartosc). 
 # Przyjmiemy, ze ciag elementow wektora (czyli ID osob) musi byc rosnacy (co nam zapewnia roznowartosciowosc).
-# Moglibysmy ewentualnie sprawdzac, czy elementy sie nie powtarzaja (to moze dac inny efekt - jestem ciekawy
-# wyniku takiego eksperymentu).
-# To jest wada uzycia GenSA - 
-# gdybysmy pisali sami te procedure to moglibysmy juz "wewnetrznie" obslugiwac zbiory danych, a nie wektory.
 
 # Bez "as.numeric" wywraca sie z komunikatem "REAL() can only be applied..."
-
-
 WYNIK <- GenSA(par=as.numeric(sample(1:N)*ROZMIAR_DANYCH/N), fn=funkcja_oceniajaca, lower=as.numeric(seq(1,N)), upper=as.numeric(seq(ROZMIAR_DANYCH-N+1,ROZMIAR_DANYCH)), FALSE, control=list(max.time=1440,smooth=FALSE,verbose=TRUE,temperature=50.0))
 print(WYNIK)
 
+#wynik
 funkcja_oceniajaca(WYNIK$par, TRUE)
-
-
-
